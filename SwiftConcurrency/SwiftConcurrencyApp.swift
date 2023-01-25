f1cacfc02b47d9b529236f7f27e6fa01f62fe07b
Roel Castano <roelinvesting@gmail.com>
2023-01-25 12:51:01 -0500

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Roel Castano on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
